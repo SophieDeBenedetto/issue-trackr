d59ea21fbb74fcdce8e4ca3e25f50ccf42ae4707
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-27 18:51:34 -0400

    show/hide repo's issue on button click on user show

--- app/assets/javascripts/repositories.js
+++ app/assets/javascripts/repositories.js
@@ -1,9 +1,18 @@
 $(function () {
+ $("div[data-show='issue']").hide();
  hideErrors();
+ toggleIssues();
 })
 
 function hideErrors(){
   $("#errors").on("click", $('[data-panel="close"]'), function(){
     $("#errors").html(" ");
+  })
+}
+
+function toggleIssues(){
+  $("[data-id='repo-list']").on("click", $("#issue-count"), function(e){
+    repoId = e.data.data().repo
+    $("[data-issue-repo='" + repoId + "']").slideToggle();
   })
 }
