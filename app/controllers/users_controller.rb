693e892e6a7487b0e39d2f8e85136c8d5fdd015d
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 15:03:23 -0400

    validate phone number format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:update]
+
   def show
     @user = current_user
   end
+
+  def update
+    if @user == current_user
+      @user.phone_number = user_params[:phone_number]
+      if @user.save
+        respond_to do |f|
+          f.js
+        end
+      else
+        respond_to do |f|
+          f.js {render 'phone_errors.js.erb'}
+        end
+      end
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:phone_number)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :repositories
   has_many :issues, through: :repositories
+  before_save :normalize_phone_number
+  # validates :url, uniqueness: true, presence: true, format: {with: /https:\/\/github.com/, message: "must be a valid github url"}
+
+  validates :phone_number, format: {with: /\d{3}-\d{3}-\d{4}/}
 
   def self.find_or_create_from_omniauth(auth)
     User.find_or_create_by(github_uid: auth["uid"]).tap do |u|
@@ -9,6 +13,16 @@ class User < ActiveRecord::Base
       u.avatar_url = auth["info"]["image"]
       u.name = auth["info"]["name"]
       u.save
+    end
+  end
+
+  def display_phone_number
+    !phone_number.empty? ? phone_number : "<i>add your phone number to receive text message updates</i>".html_safe
+  end
+
+  def normalize_phone_number
+    if phone_number && phone_number.strip.empty?
+      phone_number = nil 
     end
   end
 end
