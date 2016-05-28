2a16528b83fcceffa3d018d7fa7a8ab47736791b
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 15:27:35 -0400

    webhooks payload should find repo and assign it if payload contains new issue

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -9,6 +9,12 @@ class WebhooksController < ApplicationController
       return
     else
       issue = Issue.find_or_create_by(url: issue_params["html_url"])
+      if !issue.repository
+        url_elements= issue_params["repository_url"].split("/")
+        repo_url = "https://github.com/#{url_elements[-2]}/#{url_elements[-1]}"
+        repo = Repository.find_by(url: repo_url)
+        issue.update(repository: repo)
+      end
       issue.update(title: issue_params["title"], content: issue_params["body"], assignee: issue_params["assignee"], status: issue_params["state"])
       owner = issue.repository.user
       if owner.phone_number
@@ -16,7 +22,7 @@ class WebhooksController < ApplicationController
         @client.messages.create(
           to: owner.phone_number, 
           from: "+1 #{ENV['TWILIO_NUMBER']}",
-           body: "#{issue.name} has been updated. View it here: #{issue.url}")
+           body: "#{issue.title} has been updated. View it here: #{issue.url}")
       end
     end
   end
