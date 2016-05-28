b2d4cf584325f4b5174e54c93d0413c41ecf724a
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 16:03:47 -0400

    email updates when issue updated from webhook

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,17 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =  {
+   :address        => "smtp.gmail.com",
+   :port       =>  587,
+   :domain       =>  'localhost',
+   :user_name      => ENV["GMAIL_ADDRESS"],
+   :password     =>  ENV["GMAIL_PASSWORD"],
+   :authentication   => 'plain',
+   :enable_starttls_auto   => true
+    }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
