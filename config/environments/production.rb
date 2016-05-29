45636f81075db26466c3ae6c9654679ec01b3eb7
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 22:37:05 -0400

    add action mailer config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =  {
+   :address        => "smtp.gmail.com",
+   :port       =>  587,
+   :domain       =>  'localhost',
+   :user_name      => ENV["GMAIL_ADDRESS"],
+   :password     =>  ENV["GMAIL_PASSWORD"],
+   :authentication   => 'plain',
+   :enable_starttls_auto   => true
+    }
 end
