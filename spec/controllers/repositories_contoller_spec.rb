70540a629984ea362fa697a3b6df60d355b51f3e
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 22:10:57 -0400

    webhooks controller spec tests that new issue is created from payload, that existing issue is updated from payload

--- spec/controllers/repositories_contoller_spec.rb
+++ spec/controllers/repositories_contoller_spec.rb
@@ -10,15 +10,24 @@ RSpec.describe RepositoriesController, :type => :controller do
    end
   describe "#create" do
     it "creates a repo with the given link and information requested from github" do
-     VCR.use_cassette("create_repo") do 
+      VCR.use_cassette("create_repo") do 
         expect do
           post :create, {repository: {url: "https://github.com/sophiedebenedetto/learn-write"}}
         end.to change {Repository.count}.from(0).to(1)
         repo = Repository.first
         expect(repo.url).to eq("https://github.com/sophiedebenedetto/learn-write")
         expect(repo.name).to eq("learn-write")
-        expect(repo.issues.count).to eq(2)
       end
+    end
+
+    it "creates the correct number of issues and associates them to the new repository" do 
+      VCR.use_cassette("create_repo") do 
+         post :create, {repository: {url: "https://github.com/sophiedebenedetto/learn-write"}}
+         repo = Repository.first
+         expect(repo.issues.count).to eq(2)
+         expect(Issue.first.repository).to eq(repo)
+         expect(Issue.second.repository).to eq(repo)
+       end
     end
   end
 end
