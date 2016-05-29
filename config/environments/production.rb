7d107ac3797f3531bebd7e4e9e51b8a1ef645ab2
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 23:08:55 -0400

    debugging action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings =  {
    :address        => "smtp.gmail.com",
    :port       =>  587,
-   :domain       =>  'localhost',
+   :domain       =>  'gmail.com',
    :user_name      => ENV["GMAIL_ADDRESS"],
    :password     =>  ENV["GMAIL_PASSWORD"],
    :authentication   => 'plain',
