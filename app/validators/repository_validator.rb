c2cbd9ccd764069a11a2eb029eac6ce9abe5c0ad
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-28 22:53:41 -0400

    fix repo validation, fix new repo append to page bug

--- app/validators/repository_validator.rb
+++ app/validators/repository_validator.rb
@@ -1,6 +1,6 @@
 class RepositoryValidator < ActiveModel::Validator
   def validate(record)
-    unless record.user.github_username == record.url.split("/")[-2]
+    unless record.user.github_username.downcase == record.url.split("/")[-2].downcase
       record.errors[:ownership] << 'you must be the owner of this repo in order to add it'
     end
   end
